960799c701b182d3d5070327477f4eb18b1ab185
Muhammad Hafidz <24hafidz@mhs.usk.ac.id>
2026-06-02 17:37:58 +0700

    Update LoginPage.tsx

--- frontend/swaphour-frontend/src/pages/LoginPage.tsx
+++ frontend/swaphour-frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import gsap from "gsap";
-import Button from "../components/UI/Button";
-import Input from "../components/UI/Input";
-import ErrorAlert from "../components/UI/ErrorAlert";
-import PasswordInput from "../components/UI/PasswordInput";
+import Button from "../components/ui/Button";
+import Input from "../components/ui/Input";
+import ErrorAlert from "../components/ui/ErrorAlert";
+import PasswordInput from "../components/ui/PasswordInput";
 import Logo from "../components/ui/Logo";
 
 const LoginPage = () => {
